492b654082053440d082282492fc8e352482ab6d
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-04-15 18:53:38 -0400

    Update 0209. Minimum Size Subarray Sum.java

--- 0209. Minimum Size Subarray Sum.java
+++ 0209. Minimum Size Subarray Sum.java
@@ -12,3 +12,29 @@ class Solution {  //O(n) solution
         return 0;
     }
 }
+
+class Solution2 {   //O(nlogn) solution
+    public int minSubArrayLen(int K, int[] nums) {
+        int low = 1, high = nums.length;
+        int res = Integer.MAX_VALUE;
+        while (low <= high) {   //bin search
+            int mid = (low + high) / 2;
+            if (able(K, nums, mid)) {   
+                high = mid - 1;
+                res = Math.min(res, mid);
+            } else low = mid + 1;
+        }
+        if (res == Integer.MAX_VALUE) return 0;
+        return res;
+    }
+    public boolean able(int K, int[] nums, int len) {   //can you find a subarray of this length that will meet K?
+        int i = 0, j = 0, sum = 0;
+        while (j < len) sum += nums[j++];
+        while (j != nums.length) {
+            if (sum >= K) return true;
+            sum += nums[j++] - nums[i++];
+        }
+        if (sum >= K) return true;
+        return false;
+    }
+}
