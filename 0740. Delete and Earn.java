1bb308ce4b4c15f443970e186cdf0957a842f4b7
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-05-21 12:32:57 -0400

    Update 0740. Delete and Earn.java

--- 0740. Delete and Earn.java
+++ 0740. Delete and Earn.java
@@ -21,3 +21,20 @@ class Solution {
         return Math.max(avoid, used);
     }
 }
+
+class Solution {
+    public int deleteAndEarn(int[] nums) {  //MUCH faster
+        int[] radix = new int[10001];
+        for (int n: nums) radix[n]++; //radix sort
+        int used = 0, avoid = 0, prev = -1; //used=max profit if you took prev, avoid=max profit if you didn't
+        for (int i = 1; i != 10001; i++) {
+            if (radix[i] == 0) continue; //only process vals present in nums
+            int temp = Math.max(used, avoid);
+            if (i == prev + 1) used = avoid + radix[i] * i; //previous "used" value deleted i, so you must add to avoid
+            else used = temp + radix[i] * i; //just add to the max you can get
+            avoid = temp; //same for both cases
+            prev = i;
+        }
+        return Math.max(used, avoid);
+    }
+}
