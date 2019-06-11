1b844fdb0d210220678624dca4b705cb37ffd606
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-06-10 21:20:12 -0400

    Update 0752. Open the Lock.java

--- 0752. Open the Lock.java
+++ 0752. Open the Lock.java
@@ -1,37 +1,40 @@
-class Solution {  //TLE, will fix
+class Solution {
     public int openLock(String[] deadends, String target) {
-        HashSet<String> vis=new HashSet<>();
-        int round=0;
-        for(String d:deadends) vis.add(d);
-        Queue<String> que=new LinkedList<>();
+        HashSet < String > vis = new HashSet < > ();
+        int round = 0;
+        for (String d: deadends) vis.add(d);
+        Queue < String > que = new LinkedList < > ();
         que.add("0000");
-        while(!que.isEmpty()){
-            int size=que.size();
-            for(int j=0;j!=size;j++){
-                String str=que.poll();
-                System.out.println(str);
-                StringBuilder curr=new StringBuilder(str);
-                if(str.equals(target)) return round;
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for (int j = 0; j != size; j++) {
+                String str = que.poll();
+                // System.out.println(str);
+                StringBuilder curr = new StringBuilder(str); //may not be worth the time cost
+                if (str.equals(target)) return round;
+                if (vis.contains(str)) continue;
                 vis.add(str);
-                for(int i=0;i!=4;i++){
-                    char c=curr.charAt(i);
-                    if(c=='0'){
-                        curr.setCharAt(i,'9');
-                        if(!vis.contains(curr.toString())) que.add(curr.toString());
-                        curr.setCharAt(i,'1');  
-                        if(!vis.contains(curr.toString())) que.add(curr.toString());
-                        continue;
-                    }else if(c=='9'){
-                        curr.setCharAt(i,'8');
-                        if(!vis.contains(curr.toString())) que.add(curr.toString());
-                        curr.setCharAt(i,'0');  
-                        if(!vis.contains(curr.toString())) que.add(curr.toString());
-                        continue;
+                for (int i = 0; i != 4; i++) {
+                    char c = curr.charAt(i);
+                    if (c == '0') {
+                        curr.setCharAt(i, '9');
+                        que.add(curr.toString());
+                        curr.setCharAt(i, '1');
+                        que.add(curr.toString());
+                        curr.setCharAt(i, '0'); //for future chars in the string
+                    } else if (c == '9') {
+                        curr.setCharAt(i, '8');
+                        que.add(curr.toString());
+                        curr.setCharAt(i, '0');
+                        que.add(curr.toString());
+                        curr.setCharAt(i, '9');
+                    } else {
+                        curr.setCharAt(i, (char)(c - 1));
+                        que.add(curr.toString());
+                        curr.setCharAt(i, (char)(c + 1));
+                        que.add(curr.toString());
+                        curr.setCharAt(i, c);
                     }
-                    curr.setCharAt(i,(char)(c-1)); 
-                    if(!vis.contains(curr.toString())) que.add(curr.toString());
-                    curr.setCharAt(i,(char)(c+1));  
-                    if(!vis.contains(curr.toString())) que.add(curr.toString());
                 }
             }
             round++;
