e7acd7ad19f86b7fe62434937cfa22a5e98307c2
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-06-12 12:50:00 -0400

    Update 0757. Set Intersection Size At Least Two.java

--- 0757. Set Intersection Size At Least Two.java
+++ 0757. Set Intersection Size At Least Two.java
@@ -1,3 +1,25 @@
+class SolutionFast {
+    public int intersectionSizeTwo(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) - > { //ascending end vals
+            return a[1] - b[1];
+        });
+        int res = 0, N = intervals.length, max = -1, max2 = -1; //max's are largest 2 nums we previously added to S
+        for (int i = 0; i != N; i++) {
+            int s = intervals[i][0], e = intervals[i][1];
+            if (s > max) {
+                res += 2;   //need to add 2 new nums into S
+                max = e;
+                max2 = e - 1;
+            } else if (s > max2) {  //keep one and add 1 new num into S, update max's accordingly
+                res++;
+                max2 = max;
+                max = e;
+            }   //max's fit into this interval, no need to add anything new
+        }
+        return res;
+    }
+}
+
 class Solution {  
     public int intersectionSizeTwo(int[][] intervals) {
         Arrays.sort(intervals, (a, b) - > { //start vals are ascending, if start vals are equal, then sort end vals descendingly
