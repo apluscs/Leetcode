3d8f72d7cadb3ebf7ab5ad155ca9c916585ddb4f
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-06-15 22:14:35 -0400

    Update 0777. Swap Adjacent in LR String.java

--- 0777. Swap Adjacent in LR String.java
+++ 0777. Swap Adjacent in LR String.java
@@ -7,7 +7,7 @@ class Solution {
             while (ps != N && sar[ps] == 'X') ps++;
             while (pe != N && ear[pe] == 'X') pe++;
             // System.out.println(ps+" "+pe);
-            if (ps == N || pe == N) return true; //visited all instances of 'R' and 'L'
+            if (ps == N) return true; //visited all instances of 'R' and 'L'; pe and ps will reach N at the same time
             if (ear[pe] == 'R' && pe < ps) return false; //'R' needs to move left
             if (ear[pe] == 'L' && pe > ps) return false; //'L' needs to move right, can never happen
             ps++;
@@ -17,3 +17,19 @@ class Solution {
     }
 } //can be improved by doing charAt() instead iterating over .toCharArray() 
 //  first check for order and freq can be done in while loop
+
+class SolutionFast {
+    public boolean canTransform(String start, String end) {
+        int ps = 0, pe = 0, N = start.length();
+        while (true) {
+            while (ps != N && start.charAt(ps) == 'X') ps++;
+            while (pe != N && end.charAt(pe) == 'X') pe++;
+            // System.out.println(ps+" "+pe);
+            if (ps == N && pe == N) return true; //visited all instances of 'R' and 'L'
+            if (ps == N || pe == N || start.charAt(ps) != end.charAt(pe) || end.charAt(pe) == 'R' && pe < ps || end.charAt(pe) == 'L' && pe > ps) return false; 
+            //only one reaches N means freq is not the same; order must be the same as well; 'R' needs to move left; 'L' needs to move right, can never happen
+            ps++;
+            pe++;
+        }
+    }
+}
