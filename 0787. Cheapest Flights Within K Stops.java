3ab323f82c5cf84ebb7d208733bdecda27d39b07
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-06-18 17:50:52 -0400

    Update 0787. Cheapest Flights Within K Stops.java

--- 0787. Cheapest Flights Within K Stops.java
+++ 0787. Cheapest Flights Within K Stops.java
@@ -17,3 +17,34 @@ class Solution {
         return -1;
     }
 }
+
+class SolutionDP {
+    public int findCheapestPrice(int N, int[][] flights, int src, int des, int K) {
+        long[][] dp = new long[K + 2][N]; //dp[i][j]=cheapest price to get to city j using i-1 stops
+        for (long[] d: dp) Arrays.fill(d, Integer.MAX_VALUE);
+        dp[0][src] = 0; //row 0 is infinite except for src
+        for (int i = 1; i != K + 2; i++) { //rows are shifted down 1 to avoid index bound exception (i-1)
+            dp[i][src] = 0; //no cost to stay put
+            for (int[] f: flights) dp[i][f[1]] = Math.min(dp[i][f[1]], dp[i - 1][f[0]] + f[2]); //price to get to departure city f[0] + price of flight to city f[1]
+            // System.out.println(Arrays.toString(dp[i]));
+        }
+        return dp[K + 1][des] == Integer.MAX_VALUE ? -1 : (int) dp[K + 1][des];
+    }
+}
+
+class SolutionDP2 { //better memory by only keeping 2 dp[] at a time
+    public int findCheapestPrice(int N, int[][] flights, int src, int des, int K) {
+        long[] prev = new long[N];
+        Arrays.fill(prev, Integer.MAX_VALUE);
+        prev[src] = 0;
+        for (int i = 0; i != K + 1; i++) {
+            long[] curr = new long[N];
+            Arrays.fill(curr, Integer.MAX_VALUE);
+            curr[src] = 0;
+            for (int[] f: flights) curr[f[1]] = Math.min(curr[f[1]], prev[f[0]] + f[2]); //price to get to departure city f[0] + price of flight to city f[1]
+            // System.out.println(Arrays.toString(curr));
+            prev = curr;
+        }
+        return prev[des] == Integer.MAX_VALUE ? -1 : (int) prev[des];
+    }
+}
