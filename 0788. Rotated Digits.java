e29b113fbf296b8b305b85a3213a5ee72b5f704b
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-06-19 10:03:37 -0400

    Create 0788. Rotated Digits.java

---
+++ 0788. Rotated Digits.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int rotatedDigits(int N) {
+        int num = 2, pow = 1, res = 0;
+        while (num <= N) {
+            if (num / pow == 3 || num / pow == 4 || num / pow == 7) { //skips over large number ranges
+                num += pow;
+                // System.out.println(num);
+                continue;
+            }
+            if (isValid(num)) res++;
+            num++;
+            // System.out.println(num+", "+pow);
+            if (num / (pow * 10) != 0) pow *= 10;
+        }
+        return res;
+    }
+
+    boolean isValid(int num) {
+        boolean flag = false; //we just need to find at least 2,5,6,9 AND no 3,4,7. All 1's and 0's won't work
+        while (num != 0) {
+            int mod = num % 10;
+            if (mod == 3 || mod == 4 || mod == 7) return false;
+            else if (mod == 2 || mod == 5 || mod == 6 || mod == 9) flag = true;
+            num /= 10;
+        }
+        return flag;
+    }
+}
