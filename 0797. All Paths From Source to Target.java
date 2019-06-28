f4ef224a0b3f4c78e9b32f15fff2a30255f58c4b
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-06-28 12:15:25 -0400

    Create 0797. All Paths From Source to Target.java

---
+++ 0797. All Paths From Source to Target.java
@@ -0,0 +1,18 @@
+class Solution {
+    int N;
+    List < List < Integer >> res;
+    int[][] graph;
+    public List < List < Integer >> allPathsSourceTarget(int[][] g) {
+        N = g.length;
+        graph = g;
+        res = new LinkedList < > ();
+        helper(new LinkedList < > (), 0);
+        return res;
+    }
+    void helper(LinkedList < Integer > curr, int nd) { //must have linked here to use methods
+        curr.add(nd);
+        if (nd == N - 1) res.add(new LinkedList < > (curr)); //must be a copy
+        for (int j: graph[nd]) helper(curr, j);
+        curr.removeLast();
+    }
+}
