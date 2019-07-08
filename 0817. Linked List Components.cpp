00969f05becbd49f85f2deda821868b63760c5ec
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-07-08 09:38:09 -0400

    Create 0817. Linked List Components.cpp

---
+++ 0817. Linked List Components.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+        int numComponents(ListNode * curr, vector < int > & G) {
+            int res = 0;
+            set < int > nums;
+            for (int num: G) nums.insert(num);
+            while (curr != NULL) {
+                if (nums.find(( * curr).val) != nums.end()) res++; //found a chain of connected components
+                while (curr != NULL && nums.find(( * curr).val) != nums.end()) curr = ( * curr).next; 
+                //iterate through the chain (may skip this part if first part of linked list is not in set)
+                while (curr != NULL && nums.find(( * curr).val) == nums.end()) curr = ( * curr).next; 
+                //iterate through this component not in set to prepare for the next one
+            }
+            return res;
+        }
+};
