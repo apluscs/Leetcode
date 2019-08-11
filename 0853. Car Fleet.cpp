7e2aa059719a00a866600115af6a89cf3f4e99e9
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-08-11 18:10:10 -0400

    Create 0853. Car Fleet.cpp

---
+++ 0853. Car Fleet.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    public:
+        int carFleet(int target, vector < int > & position, vector < int > & speed) {
+            int N = speed.size(), res = 0;
+            double timeFirst = -1; //time it takes first car in curr fleet to reach target
+            vector < vector < int >> cars(N);
+            for (int i = 0; i != N; i++) {
+                cars[i].push_back(position[i]); //sort by position closest to target
+                cars[i].push_back(speed[i]);
+            }
+
+            sort(cars.begin(), cars.end(), compareArr);
+            // for(int i=0;i!=N;i++) cout<<cars[i][0]<<", "<<cars[i][1]<<endl;
+            for (int i = 0; i != N; i++) {
+                double curr = (target - cars[i][0]) / (double) cars[i][1];
+                if (curr > timeFirst) {
+                    res++; //tails behind curr fleet, leading a new fleet
+                    timeFirst = curr;
+                    // cout<<timeFirst<<'\t';
+                }
+            }
+            return res;
+        }
+
+    static bool compareArr(vector < int > & car1, vector < int > & car2) {
+        return car1[0] > car2[0];
+    }
+};
