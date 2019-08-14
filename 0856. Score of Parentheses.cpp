f2efe461a6858cee84b8448003cab2ba0a1a00fd
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-08-14 10:20:28 -0400

    Update 0856. Score of Parentheses.cpp

--- 0856. Score of Parentheses.cpp
+++ 0856. Score of Parentheses.cpp
@@ -1,6 +1,25 @@
 class Solution {
     public:
         int scoreOfParentheses(string S) {
+            int res = 0, layer = 1, N = S.length();
+            for (int i = 0; i != N; i++) {
+                char c = S[i];
+                if (c == '(') layer << 1; //how many ( ) are around the to-be core
+                else {
+                    // cout<<c<<", "<<i;
+                    layer >> 1;
+                    char prev = S[i - 1];
+                    if (prev == '(') res += layer; //found a core, adding 2^(layer-1) to res
+                }
+                // cout<<layer<<'\t';
+            }
+            return res;
+        }
+};
+
+class SolutionBadMemory {
+    public:
+        int scoreOfParentheses(string S) {
             stack < int > st;
             st.push(0); //st at ith level = the value contained inside the ith level of ( )
             for (char c: S) {
