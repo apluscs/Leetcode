ef1aa5560ecdb5f40c85cbebcd60e9520c5b098e
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-11-16 16:08:31 -0500

    Create 0070. Climbing Stairs.cpp

---
+++ 0070. Climbing Stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int climbStairs(int n) {
+            int prev1 = 1, prev2 = 1;
+            if (n < 2) return 1;
+            for (int i = 2; i != n + 1; i++) {
+                int temp = prev1;
+                prev1 += prev2;
+                prev2 = temp;
+            }
+            return prev1;
+        }
+};
