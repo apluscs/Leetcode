296f7be4a2f47e37812e18fa0d49cb0f1b3139f9
Claire Yang <34817835+apluscs@users.noreply.github.com>
2019-12-09 16:14:35 -0500

    Update 0377. Combination Sum IV.cpp

--- 0377. Combination Sum IV.cpp
+++ 0377. Combination Sum IV.cpp
@@ -12,3 +12,11 @@ class Solution {
             return dp[target];
         }
 };
+
+/* Explanation to first question:
+
+I think the problem is, the combinations from 1 to target could be too large to be represent by any number type. 
+The unsigned int or any unsigned type just automatically overflow to modulo "type max value". 
+For example, unsigned int x = 4294967295 is ok but after do x += 1 operation, x will be 0 without any runtime error.
+
+*/
