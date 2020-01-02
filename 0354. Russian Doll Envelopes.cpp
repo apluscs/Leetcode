dc2e9942e8c7eb7122533c03f23daef822ade54c
Claire Yang <34817835+apluscs@users.noreply.github.com>
2020-01-02 10:45:12 -0500

    Update 0354. Russian Doll Envelopes.cpp

--- 0354. Russian Doll Envelopes.cpp
+++ 0354. Russian Doll Envelopes.cpp
@@ -1,20 +1,24 @@
+        
 class Solution {
-    public:
-        static bool Compar(const vector < int > & a,
-            const vector < int > & b) {
-            return a[0] != b[0] ? a[0] < b[0] : a[1] > b[1];
-        } // note: these things need to be static, parameters need to be const
-
-    int maxEnvelopes(vector < vector < int >> & evs) {
-        int N = evs.size(), res = 0;
-        sort(evs.begin(), evs.end(), Compar); // ascending width, descending height
-        vector < int > dp(N);
-        for (int i = 0; i != N; i++) { // LIS
-            vector < int > curr = evs[i];
-            int j = lower_bound(dp.begin(), dp.begin() + res, curr[1]) - dp.begin();
-            dp[j] = curr[1]; // 
-            if (j == res) res++;
+public:
+    static bool Compar(const vector<int>& a, const vector<int>& b)  {
+        return a[0]!=b[0]? a[0]<b[0]: a[1]>b[1];
+    }   // note: these things need to be static, parameters need to be const
+    
+    int maxEnvelopes(vector<vector<int>>& evs) {
+        int N=evs.size(), res=0;
+        sort(evs.begin(), evs.end(), Compar);   // ascending width, descending height
+        vector<int> dp(N);
+        for(int i=0;i!=N;i++){  // LIS- since it ascends upward by width, that's already guaranteed to be >= increasing
+            vector<int> curr=evs[i];
+            int j=lower_bound(dp.begin(), dp.begin()+res, curr[1])-dp.begin();
+            dp[j]=curr[1];  // replace the higher option if possible
+            if(j==res) res++;
         }
         return res;
-    }
+    }  
 };
+
+
+
+
