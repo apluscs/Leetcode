13cb93aca87c517af489f849cf5186fb7cd8e432
Claire Yang <34817835+apluscs@users.noreply.github.com>
2020-01-31 11:29:18 -0500

    Create 0617. Merge Two Binary Trees.cpp

---
+++ 0617. Merge Two Binary Trees.cpp
@@ -0,0 +1,22 @@
+// Try iterative approach next time
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(t1 == nullptr) return t2;    // will return nullptr if both are nullptrs
+        if(t2 == nullptr) return t1;
+        t1->val += t2->val; // t1 will store the result
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
+    }
+};
