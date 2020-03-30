981f56672fa4d5d64d7e4ea7cb69c988c64deefe
Claire Yang <34817835+apluscs@users.noreply.github.com>
2020-03-29 20:19:01 -0400

    Update 0611. Valid Triangle Number.cpp

--- 0611. Valid Triangle Number.cpp
+++ 0611. Valid Triangle Number.cpp
@@ -1,32 +1,20 @@
-class Solution {  // definitely wrong
+class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        int res = 0, N = nums.size(), j = 0;    // j is a pointer in nums
-        vector<int> starts(1004);
-        for(int i = 0; i != 1001; ++i){
-            starts[i] = j;
-            while(j < N-1 && nums[j] == i && nums[j+1] == i)
-                j++;
-            if(j != N && nums[j] == i)
-                j++;
-
-        }
-        for(int i = 0; i != 6; ++i)
-            cout << starts[i] << " ";
-        cout << endl;
-        for(int i = 0; i != N; ++i){
-            for(j = i + 1; j < N; ++j){ // nums[j] > nums[i]
-                int low = nums[j] - nums[i] + 1, high = nums[j] + nums[i] - 1;
-                res += starts[high + 1] - starts[low];
-                if(nums[j] >= low && nums[j] <= high) res--;
-                if(nums[i] >= low && nums[i] <= high) res--; 
-                cout << nums[i] << ", " << nums[j] << ", " << res << ", " << low << ", " << high << endl;
-                while(j < N-1 && nums[j+1] == nums[j]) j++;
+        int res = 0, N = nums.size();  
+        for(int i = N - 1; i >= 2; --i){    // nums[i] is the target
+            int low = 0, high = i - 1;
+            while(low < high){
+                if(nums[low] + nums[high] > nums[i]){
+                    res += high - low;  // everything between low and high 
+                    high--;
+                } else {
+                    low++;
+                }
             }
-            while(i < N-1 && nums[i+1] == nums[i]) i++;
         }
-        return res / 2;
+        return res;
     }
     
 };
