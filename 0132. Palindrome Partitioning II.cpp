8780c1005d4beb527912ae7136f12eed2177d83e
Claire Yang <34817835+apluscs@users.noreply.github.com>
2020-04-05 17:24:14 -0400

    Update 0132. Palindrome Partitioning II.cpp

--- 0132. Palindrome Partitioning II.cpp
+++ 0132. Palindrome Partitioning II.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
-    vector<vector<bool>> valid;
-    vector<vector<int>> dp;
-    int res;
+    
     int minCut(string s) {
-        valid.clear();
-        dp.clear();
-        res = INT_MAX;
         int N = s.length();
         if(N == 0) return 0;
-        valid.resize(N, vector<bool>(N, true));
-        dp.resize(N, vector<int>(N, -1));
-        for(int gap = 1; gap != N; ++gap){
+        vector<vector<bool>> valid(N, vector<bool>(N, true));
+
+        // fills upper right half of valid, goes along diagonals
+        for(int gap = 1; gap != N; ++gap){  
             for(int i = 0; i + gap != N; ++i){
                 int j = i + gap;
-                valid[i][j] = s[i] == s[j] && valid[i+1][j-1];    // ends have to match, and so do interiors
+                // ends have to match, and so do interiors
+                valid[i][j] = s[i] == s[j] && valid[i+1][j-1];    
             }
         }
-        return helper(0, N - 1) - 1;    // minus the last cut
-    }
-    
-    int helper(int start, int end){ // returns min #cuts needed for this inclusive range (including the one at the very end)
-        if(start > end) return 0;
-        if(dp[start][end] != -1) return dp[start][end];
-        int res= INT_MAX;
-        for(int i = start; i <= end; ++i){
-            if(valid[start][i])
-                res = min(res, 1 + helper(i+1, end));
+        vector<int> dp(N, INT_MAX); // dp[i] = minCuts from start of s, to i, inclusive
+        for(int i = 0; i != N; ++i){
+            for(int j = i; j >= 0; --j){    
+                if(valid[j][i]){    // if range from j to i is palindrome
+                    int pre = j == 0 ? 0 : dp[j - 1];   // if j == 0, nothing came before it
+                    dp[i] = min(dp[i], pre + 1);    // j through i is a palindrome, how many cuts needed for area before?
+                }
+            }
         }
-        dp[start][end] = res;
-        return res;
+        return dp[N-1] - 1; // minus the cut at the "end"
+        
     }
 };
