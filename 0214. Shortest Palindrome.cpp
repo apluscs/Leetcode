6810d5cc082da79533a883443c07492bc80f1adc
Claire Yang <34817835+apluscs@users.noreply.github.com>
2020-04-25 19:57:29 -0400

    Update 0214. Shortest Palindrome.cpp

--- 0214. Shortest Palindrome.cpp
+++ 0214. Shortest Palindrome.cpp
@@ -1,4 +1,30 @@
-class Solution {
+class Solution {    // KMP much faster
+public:
+    string shortestPalindrome(string s) {
+        int M=s.length();
+        string t=s;
+        reverse(t.begin(),t.end());
+        string s_new=s+"#"+t;
+        // cout<<s_new;
+        int N=s_new.length();
+        vector<int>dp(N);
+        for(int i=1;i!=N;++i){
+            int x=dp[i-1];  // current suffix matches with dp[i-1] of prefix, can we cont?
+            while(x >0&&s_new[i] != s_new[x]){
+                x=dp[x-1];    // even if you can't continue this one, this suffix matches with dp[x] of the prefix, so try there
+            }
+            if(s_new[x] == s_new[i]) x++;
+            dp[i]=x;
+        }   // how much of prefix is a palindrome? indicated by how much of it, reflected, matches it, originally... the reflected part is now at the end, as a "suffix" of s_new
+        int x=dp[N-1];
+        t=s.substr(x);
+        reverse(t.begin(),t.end());
+        return t+s;
+    }
+};
+
+
+class SolutionSLOW {
 public:
     string shortestPalindrome(string s) {
         string t = "$#";
