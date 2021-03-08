e58b125a793880e4957c14e011c06c58b3393a1f
Claire Yang <34817835+apluscs@users.noreply.github.com>
2021-03-07 23:33:34 -0500

    Create 1782. Count Pairs Of Nodes.cpp

---
+++ 1782. Count Pairs Of Nodes.cpp
@@ -0,0 +1,61 @@
+#define print_arr(arr, _n) \
+  {for(int _i =0; _i<_n; ++_i)               \
+  cout << arr[_i] << " ";  \
+  cout << endl; }
+class Solution {
+public:
+  int total[20001] = {0}, n, tree[100001];
+  unordered_set<int> adj[20001];
+  vector<int> countPairs(int n, vector<vector<int>>& edges, vector<int>& queries) {
+    vector<int> res;
+    this->n = n;
+    unordered_map<pair<int,int>, int, pair_hash> between;  // between = #edges directly between (u,v)
+    for(auto& e : edges){
+      if(e[0] > e[1]) swap(e[0], e[1]);
+      between[{e[0], e[1]}]++, total[e[0]]++, total[e[1]]++, adj[e[0]].insert(e[1]);
+    } 
+    // print_arr(total, n+1);
+    for(int q : queries) {
+      int curr = 0;
+      for(auto& p : between) {  // pairs with direct edge in between
+        int u = p.first.first, v = p.first.second, b = p.second;
+        curr += total[u] + total[v] - b > q;
+      }
+      // cout << curr << endl;
+      res.push_back(curr + solve(q));
+    }
+    return res;
+  }
+  int solve(int q) {  // want to know pairs without a direct edge that have total[u] + total[v] > q
+    int res = 0;
+    memset(tree, 0, sizeof(tree));
+    for(int i=1; i<=n; ++i) {
+      update(total[i], 1);
+    }
+    for(int i=1; i<=n; ++i) {
+      update(total[i], -1); // things to remove: yourself, all direct-edged neighbors
+      for(int j : adj[i]) update(total[j], -1);
+      res += n - i - adj[i].size() - query(q - total[i]); // how many are in the tree?
+      // res += query(100000) - query(q - total[i]); // how many are in the tree?
+      for(int j : adj[i]) update(total[j], 1);
+    }
+    return res;
+  }
+  void update(int v, int d) {  // d is the incoming change
+    for (int i = v; i <= 100000; i += (i & -i)) tree[i] += d;
+  }
+  int query(int v) {
+    int res = 0;
+    for (int i = v; i>0; i -= (i & -i)) res += tree[i];
+    return res;
+  }
+  struct pair_hash {
+    template <class T1, class T2>
+    size_t operator()(pair<T1, T2> const& pair) const {
+      size_t h1 = std::hash<T1>()(pair.first);
+      size_t h2 = std::hash<T2>()(pair.second);
+
+      return h1 ^ h2;
+    }
+  };
+};
