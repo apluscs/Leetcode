fb81184a21f3f1f86ce12e4ea123e8912e57e844
apluscs <34817835+apluscs@users.noreply.github.com>
2023-01-09 20:32:45 -0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,26 @@
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+#define ll long long
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
+#define FORD(a, b, c) for (int(a) = (b); (a) >= (c); --(a))
+#define FOREACH(a, b) for (auto&(a) : (b))
+#define REP(i, n) FOR(i, 0, n)
+#define REPN(i, n) FORN(i, 1, n)
+#define dbg(x) cout << (#x) << " is " << (x) << endl;
+#define dbg2(x, y) cout << (#x) << " is " << (x) << " and " << (#y) << " is " << y << endl;
+#define dbgarr(x, sz)                                             \
+  for (int asdf = 0; asdf < (sz); asdf++) cout << x[asdf] << ' '; \
+  cout << endl;
+#define dbgarr2(x, rose, colin)                \
+  for (int asdf2 = 0; asdf2 < rose; asdf2++) { \
+    dbgarr(x[asdf2], colin);                   \
+  }
+#define dbgitem(x)                                                           \
+  for (auto asdf = x.begin(); asdf != x.end(); asdf++) cout << *asdf << ' '; \
+  cout << endl;
